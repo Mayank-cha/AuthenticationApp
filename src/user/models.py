f78e918298964dbb763a2ca082ded5fab0ffcbc2
Varun <varunpatel207@gmail.com>
2022-09-24 22:50:04 -0400

    - google auth login added

--- src/user/models.py
+++ src/user/models.py
@@ -1,28 +1,25 @@
 from django.db import models
-from django.db.models import Q
 
 
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(null=True)
-    phone = models.CharField(max_length=13, unique=True, null=True)
+    phone = models.CharField(max_length=13, null=True)
     email = models.EmailField(unique=True, null=False)
-    password = models.TextField(null=False)
+    password = models.TextField(null=True) # password null for google auth login
     is_verified = models.BooleanField(null=True, default=False)
+    google_user_id = models.CharField(max_length=100, null=True)
 
     @classmethod
     def search_user(cls, **kwargs):
-        phone = kwargs.get('phone')
         email = kwargs.get('email')
+        google_user_id = kwargs.get('google_user_id')
 
         user_query = User.objects
 
-        if phone and email:
-            return user_query.filter(Q(phone=phone) | Q(email=email)).first()
-
-        if phone:
-            return user_query.filter(phone=phone).first()
+        if google_user_id:
+            return user_query.filter(google_user_id=google_user_id).first()
 
         if email:
             return user_query.filter(email=email).first()
--- src/user/views.py
+++ src/user/views.py
@@ -1,9 +1,10 @@
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
 from helpers.constants import Response
 from helpers.helper import hash_password, verify_password
-from helpers.responses import data_not_acceptable, response_success
+from helpers.responses import data_not_acceptable, response_success, custom_response
 from user.models import User
 
 
@@ -15,33 +16,44 @@ class UserViews:
         phone = request.POST.get('phone')
         email = request.POST.get('email')
         password = request.POST.get('password')
+        google_user_id = request.POST.get('google_user_id')
 
-        user_object = User.search_user(email=email, phone=phone)
+        user_object = User.search_user(email=email, google_user_id=google_user_id)
 
         if user_object:
-            return data_not_acceptable(Response.ALREADY_EXIST.value.format("User with given phone or email"))
+            return data_not_acceptable(Response.ALREADY_EXIST.value.format("User with given credentials"))
 
-        user_object = User(
-            name=name,
-            bio=bio,
-            phone=phone,
-            email=email
-        )
-        user_object.password = hash_password(plain_text=password)
-        user_object.save()
+        try:
+            user_object = User(
+                name=name,
+                bio=bio,
+                phone=phone,
+                email=email,
+                google_user_id=google_user_id
+            )
+            if password:
+                user_object.password = hash_password(plain_text=password)
+            user_object.save()
+        except Exception as e:
+            return custom_response(status.HTTP_200_OK, False, message=Response.INVALID_INFORMATION.value, data={})
 
         return response_success(Response.ADD_SUCCESS.value.format("User"), {})
 
     @api_view(['POST'])
     def login(request, **kwargs):
-        email = request.data.get('email')
-        password = request.data.get('password')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        google_user_id = request.POST.get('google_user_id')
 
-        user_object = User.search_user(email=email)
+        user_object = User.search_user(email=email, google_user_id=google_user_id)
 
-        if user_object:
+        if user_object and google_user_id:
+            return response_success(Response.LOGIN_SUCCESS.value, {})
+
+        if user_object and password:
             if verify_password(plain_text=password, hashed_password=user_object.password):
                 return response_success(Response.LOGIN_SUCCESS.value, {})
             else:
                 return data_not_acceptable(Response.INCORRECT_PASSWORD.value)
+
         return data_not_acceptable(Response.NOT_EXIST.value.format("User"))
